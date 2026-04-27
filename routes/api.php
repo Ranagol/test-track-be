3f36139285469a6ed862ca01ad83b60c5d6273e1
Andor Horvat <andorhorvat@gmail.com>
2026-04-27 12:23:03 +0200

    Fix Xdebug - done.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Http\Controllers\Api\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('tests', TestController::class);
+
+Route::get('/xxx', function (Request $request) {
+    $t = 8;
+
+    return response()->json(
+        [
+            'message' => 'API is working',
+            'user' => $request->user(),
+        ]
+    );
 });
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    xdebug_info();
+    exit;
+
     return ['Laravel' => app()->version()];
 });
 
