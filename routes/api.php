5e38629c64a7b41069cbc314a461a45326b6f948
Andor Horvat <andorhorvat@gmail.com>
2026-05-02 13:35:13 +0200

    Test commit: tests git hooks and github actions

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// This route is for testing purposes only, it should be removed in production
 Route::get('/xxx', function (Request $request) {
 
     $t = 8;
