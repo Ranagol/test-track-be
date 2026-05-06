e2b35df64616b18f7afe16c034496ea1e8e13974
Andor Horvat <andorhorvat@gmail.com>
2026-05-06 16:58:40 +0200

    Do template for Register.vue

--- test-track-fe/src/views/login/Login.vue
+++ test-track-fe/src/views/login/Login.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="grid place-items-center">
         <h1 class="text-2xl mt-24">Login</h1>
-        <el-form class="w-full max-w-sm mt-3">
-            <el-form-item label="Username">
-                <el-input v-model="username" placeholder="Enter your username"></el-input>
+        <el-form class="w-full max-w-sm mt-3" label-width="8rem" label-position="top">
+            <el-form-item label="Email">
+                <el-input v-model="email" placeholder="Enter your email"></el-input>
             </el-form-item>
             <el-form-item label="Password">
                 <el-input v-model="password" type="password" placeholder="Enter your password"></el-input>
@@ -18,11 +18,11 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const username = ref('');
+const email = ref('');
 const password = ref('');
 
 const handleLogin = () => {
-    console.log('Login clicked', username.value, password.value);
+    console.log('Login clicked', email.value, password.value);
 };
 </script>
 
--- test-track-fe/src/views/register/Register.vue
+++ test-track-fe/src/views/register/Register.vue
@@ -1,11 +1,39 @@
 <template>
-    <h1>Register</h1>
-
+    <div class="grid place-items-center">
+        <h1 class="text-2xl mt-24">Register</h1>
+        <el-form class="w-full max-w-sm mt-3" label-width="8rem" label-position="top">
+            <el-form-item label="Name">
+                <el-input v-model="name" placeholder="Enter your name"></el-input>
+            </el-form-item>
+            <el-form-item label="Email">
+                <el-input v-model="email" type="email" placeholder="Enter your email"></el-input>
+            </el-form-item>
+            <el-form-item label="Password">
+                <el-input v-model="password" type="password" placeholder="Enter your password"></el-input>
+            </el-form-item>
+            <el-form-item label="Confirm password">
+                <el-input v-model="confirm_password" type="password" placeholder="Confirm your password"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button class="ml-auto" type="primary" @click="handleRegister">Register</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const name = ref('');
+const email = ref('');
+const password = ref('');
+const confirm_password = ref('');
+
+const handleRegister = () => {
+    console.log('Register clicked', name.value, email.value, password.value, confirm_password.value);
+};
 </script>
+
 
 <style scoped>
 
