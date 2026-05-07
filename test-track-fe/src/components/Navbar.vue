b92f83e23e01ce4230bb85fee07a407b40aa1fdd
Andor Horvat <andorhorvat@gmail.com>
2026-05-07 11:39:46 +0200

    Fix Navbar color

--- test-track-fe/src/components/Navbar.vue
+++ test-track-fe/src/components/Navbar.vue
@@ -4,9 +4,7 @@
         class="el-menu-demo"
         mode="horizontal"
         router
-        background-color="#545c64"
-        text-color="#fff"
-        active-text-color="#ffd04b"
+        
     >
         <el-menu-item index="/">Home</el-menu-item>
         <el-menu-item index="/tests">Tests</el-menu-item>
