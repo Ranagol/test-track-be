897c1183b21b36b0ea540a36fb544be32d1e6c77
Andor Horvat <andorhorvat@gmail.com>
2026-05-08 21:46:23 +0200

    Small corrections

--- test-track-fe/src/App.vue
+++ test-track-fe/src/App.vue
@@ -14,8 +14,16 @@ import { getCsrfCookie } from '@/services/authService';
 
 let authStore = useAuthStore();
 
+/**
+ * We always want to have a fresh CSRF cookie. So whenever this app loads, we get a new CSRF cookie.
+ * The currently logged in user is stored in Pinia store. Pinia store forgets the currently logged in
+ * user of refresh. The user seems logged out after refresh on FE, but on BE the user is still logged
+ * in. So, to solve this, we simply get the currently logged in user from BE on every refresh.
+ * If at the very beginning there is no currently logged in user, then the user in Pinia will be null.
+ */
 onMounted(async () => {
     await getCsrfCookie();
+    await authStore.getCurrentUser();
 });
 
 
