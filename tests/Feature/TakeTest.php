c6671d657409802dfacdd039ed7842d8eedd59e0
Andor Horvat <andorhorvat@gmail.com>
2026-06-10 12:55:21 +0200

    Add Larastan ignore element

--- tests/Feature/TakeTest.php
+++ tests/Feature/TakeTest.php
@@ -19,6 +19,8 @@ class TakeTest extends TestCase
         parent::setUp();
 
         $this->seed([
+
+            // TODO ANDOR we need to get rid of the Spatie Permissions
             RolesAndPermissionsSeeder::class,
             UserSeeder::class,
             RealTestSeeder::class,
