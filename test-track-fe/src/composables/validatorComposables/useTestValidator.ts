b790cc1d0fabf3324173b61909b2c37372c18a5a
Andor Horvat <andorhorvat@gmail.com>
2026-06-24 19:18:31 +0200

    Fix update BE validation issue

--- test-track-fe/src/composables/validatorComposables/useTestValidator.ts
+++ test-track-fe/src/composables/validatorComposables/useTestValidator.ts
@@ -1,32 +1,41 @@
 import { ref, type Ref } from 'vue';
 import type { FormInstance } from 'element-plus';
 
+/**
+ * Validates the test form on both frontend and backend levels. Because our form could have
+ * validation errors both from BE and FE.
+ * As a validator, it returns true if validation passes, false if there are validation errors.
+ */
 export function useTestValidator(
     validationRef: Ref<FormInstance | undefined>,
-    backendValidationErrors: object
-): {
-    validateTest: () => Promise<boolean>;
-} {
+    backendValidationErrors: Record<string, any>
+) {
 
-    /**
-     * Validates the test form on both frontend and backend levels.
-     * Returns true if validation passes, false if there are validation errors.
-     */
+    // We use async, because validation is async in Element Plus
     const validateTest = async (): Promise<boolean> => {
 
-        // Reset all validation errors from BE.
-        backendValidationErrors = {}
-
-        // Reset all validation errors on FE.
-        validationRef.value?.clearValidate()
-
+        // If the form is not mounted yet, we can't validate it, so we return false.
         if (!validationRef.value) {
-            return false
+            return false;
         }
 
-        const valid = await validationRef.value.validate()
+        // Reset all validation errors from BE, by looping through the keys of the object
+        Object.keys(backendValidationErrors).forEach(
+            key => { delete backendValidationErrors[key]}
+        );
 
-        return valid
+        // Reset all validation errors on FE, with the default clearValidate() method of Element Plus
+        validationRef.value?.clearValidate();
+
+        /**
+         * We use try, because the validate() method of Element Plus can throw an error.
+         */
+        try {
+            const valid = await validationRef.value.validate();
+            return valid;
+        } catch {
+            return false;
+        }
     };
 
     return {
