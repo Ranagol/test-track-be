7d0187a55e35924c02ff15d6503ee8855aa6f789
Andor Horvat <andorhorvat@gmail.com>
2026-08-06 11:17:40 +0200

    Remove login details from Login component

--- test-track-fe/src/views/home/Home.vue
+++ test-track-fe/src/views/home/Home.vue
@@ -3,34 +3,9 @@
     <h1 class="text-3xl font-bold underline">
         Welcome to Test Track!
     </h1>
-
-    <el-form
-        class="mt-3"
-        label-position="top"
-        style="width: 33%"
-    >
-        <el-form-item
-            label="Please type in your test code to start the test"
-        >
-            <el-input
-                placeholder="Test code"
-                v-model="data.testCode"
-            />
-        </el-form-item>
-
-    </el-form>
-
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue';
-
-
-let data = reactive({
-    testCode: '',
-});
-
-
 
 </script>
 
--- test-track-fe/src/views/login/Login.vue
+++ test-track-fe/src/views/login/Login.vue
@@ -2,10 +2,6 @@
     <div class="grid place-items-center">
 
         <h1 class="text-2xl mt-24">Login</h1>
-        <h1>test-taker@gmail.com</h1>
-        <h1>admin@gmail.com</h1>
-        <h1>tester@gmail.com</h1>
-
 
         <el-form
             ref="formRef"
