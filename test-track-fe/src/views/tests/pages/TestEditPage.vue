8735905a62b656872462d67b6b53e82c61c77464
Andor Horvat <andorhorvat@gmail.com>
2026-08-15 19:29:41 +0200

    Remove not needed comments

--- test-track-fe/src/views/tests/pages/TestEditPage.vue
+++ test-track-fe/src/views/tests/pages/TestEditPage.vue
@@ -105,7 +105,6 @@ const updateTest = async () => {
         await testEditorStore.update();
         ElMessage.success('Test updated');
     } catch (e) {
-        console.log('This was triggered, but it should not be');//This line is triggered, when there is a form frontend validation error. How?
         handleBackendErrors(e);
     }
 };
