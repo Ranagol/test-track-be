e2c964d5881abc827821c34a6913736a20688377
Andor Horvat <andorhorvat@gmail.com>
2026-08-16 09:43:30 +0200

    Fix small router waiting issue

--- test-track-fe/src/main.ts
+++ test-track-fe/src/main.ts
@@ -4,13 +4,20 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
-// My custom CSS. This must be last imported, so it can override the default styles of Element Plus and Tailwind CSS if needed.
+
+/**
+ * My custom CSS. This must be last imported, so it can override the default styles of Element Plus
+ * and Tailwind CSS if needed.
+ */
 import './assets/main.css'
 import VueApexCharts from "vue3-apexcharts";
 
 const app = createApp(App);
+
 app.use(ElementPlus);
 app.use(router);
 app.use(createPinia());
 app.use(VueApexCharts);
+
+await router.isReady();
 app.mount('#app');
