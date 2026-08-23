13988cb2809e7bde4b84478d3e09e0246ac725c7
Andor Horvat <andorhorvat@gmail.com>
2026-08-23 13:08:45 +0200

    Install Resend package for email sending

--- app/Mail/NewUserRegisteredMail.php
+++ app/Mail/NewUserRegisteredMail.php
@@ -5,7 +5,6 @@ namespace App\Mail;
 use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -45,15 +44,5 @@ class NewUserRegisteredMail extends Mailable
                 'user' => $this->user,
             ],
         );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
     }
 }
